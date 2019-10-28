0b391c2cb2dfabe6e25bbdfddb405c712cbd009f
celvira <clement.elvira@inria.fr>
2019-10-28 18:58:30 +0100

    Import xp 1

---
+++ experiments/TSP2019/A_radius/exp.py
@@ -0,0 +1,175 @@
+import yaml, os
+import numpy as np
+
+import pickle
+import sys
+sys.path.insert(0,'antisparse_screening')
+from dictionaries import sample_dictionary
+from sinatra_rescaled import sinatra_rescaled
+from sinatra_fw import frank_wolfe_antisparse
+
+from utils import printProgressBar
+
+FOLDER = "experiments/TSP/A_radius/"
+
+
+def _solve(yObs, matA, lbd, gap, maxIter):
+    '''
+        Find a "high accuracy" solution of the antisparse problem
+        Use frank wolfe
+    '''
+    stopping = {"max_iter": maxIter, "gap_tol":gap, 'bprint': False}
+    #(x_out, _) = frank_wolfe_antisparse(matA, yObs, lbd, stopping)
+    (x_out, _) = sinatra_rescaled(matA, yObs, lbd, stopping)
+
+    return x_out
+
+
+def _run_one_vec(yObs, matA, vecx, nbPoint, lbd):
+    '''
+        Given the solution of the antisparse problem,
+        compute the vector of tests for various radius of gap sphere
+        (centered in u^\\star).
+    '''
+    vecu = yObs - matA @ vecx
+    vecu_admissible = vecu / np.linalg.norm(matA.transpose() @ vecu, 1) 
+
+    # Compute Gap
+    primal = .5 * np.linalg.norm(vecu, 2)**2 + lbd * np.linalg.norm(vecx, np.Inf)
+    dual   = .5 * (np.linalg.norm(yObs, 2)**2 - np.linalg.norm(yObs - lbd * vecu_admissible, 2)**2)
+    gap = primal - dual
+    if gap < 0:
+        # may happen
+        gap = 1e-15
+
+    # Perform test
+    r_gap = np.sqrt(2 * gap) / lbd
+    range_radius = np.linspace(0.001, 2, nbPoint)
+    n = matA.shape[1]
+
+    r_st1 = np.linalg.norm(yObs - lbd * vecu_admissible, 2)
+
+    test_gap = 0 * range_radius
+    test_st1 = 0 * range_radius
+    for i, r in enumerate(range_radius):
+        # -- Test: |A^tc| > r || a_i || ?
+        test_gap[i] = np.sum(np.abs(matA.transpose() @ vecu) > r_gap + r)
+        test_st1[i] = np.sum(np.abs(matA.transpose() @ yObs) > r_st1 + r)
+
+    return test_gap, test_st1, gap
+
+
+def _one_run(type_dico, m, n, ratiolbd, gap, nbIt, nbPoint, tol_saturation):
+    '''
+
+    '''
+    matA = sample_dictionary(type_dico, m, n, True)
+    yObs = np.random.randn(m)
+
+    lambda_max = np.linalg.norm(matA.transpose() @ yObs, 1)
+    lbd = ratiolbd * lambda_max
+
+    vecx = _solve(yObs, matA, lbd, gap, nbIt)
+    sat_gap, sat_ST1, gap = _run_one_vec(yObs, matA, vecx, nbPoint, lbd)
+
+    nb_sat = _detect_nb_sat(vecx, tol_saturation)
+    sat_gap /= float(nb_sat)
+    sat_ST1 /= float(nb_sat)
+
+    return sat_gap, sat_ST1, gap
+
+
+def _detect_nb_sat(vecx, tol):
+    '''
+    '''
+    xmax = np.linalg.norm(vecx, np.Inf)
+    nb_sat = np.sum(np.abs(vecx) - xmax + tol > 0)
+
+    return nb_sat
+
+
+if __name__ == '__main__':
+    # 1. Read configuration file
+    with open(FOLDER + 'exp.yaml', 'r') as stream:
+        try:
+            configuration = yaml.load(stream, Loader=yaml.Loader)
+        except yaml.YAMLError as exc:
+            raise exc
+
+    expName  = str(configuration['expName'])
+    nbRepet   = int(configuration['nbRepet'])
+    
+    dualgap = float(configuration['dualgap'])
+    nbIt    = int(configuration['nbIt'])
+    tol_saturation = float(configuration['tol_saturation'])
+    
+    listM = configuration['listM']
+    listN = configuration['listN']
+    listLbd = configuration['listLambda_over_lambdaMax']
+    listDico = configuration['listDico']
+
+    nbPoint = int(configuration['nbPoints'])
+
+    version = str(configuration['version'])
+
+
+    results_gap = np.zeros( (len(listDico), len(listM), len(listLbd), nbPoint) )
+    results_st1 = np.zeros( (len(listDico), len(listM), len(listLbd), nbPoint) )
+
+    for i_dico in range(len(listDico)):
+        dico = str(listDico[i_dico])
+        print("Dictionary " + str(dico))
+        for i_size in range(len(listM)):
+            m = int(listM[i_size])
+            n = int(listN[i_size])
+            for i_lbd in range(len(listLbd)):
+                lbd = float(listLbd[i_lbd])
+                print("[m,n] = " + str([m,n]) + " lbd=" + str(lbd) + str("lbd_max"))
+                printProgressBar(0, nbRepet, \
+                    prefix = 'Progress:', suffix = 'Complete', length = 25)
+
+                vec_gap = np.zeros(nbRepet)
+                for rep in range(nbRepet):
+                    nbPb = 0
+                    # try:
+                    sat_gap, sat_ST1, gap = _one_run(dico, m, n, lbd, dualgap, nbIt, nbPoint, tol_saturation)
+                    results_gap[i_dico, i_size, i_lbd, :] += sat_gap
+                    results_st1[i_dico, i_size, i_lbd, :] += sat_ST1
+                    vec_gap[rep] = gap
+                    # except Exception as e:
+                    #     nbPb + 1
+
+                    printProgressBar(rep+1, nbRepet, \
+                        prefix = 'Progress:', suffix = 'Complete', length = 25)
+
+                print("Vector of dual gaps")
+                print(vec_gap)
+                print("")
+
+                #results[i_dico, i_size, i_lbd, :] /= float(nbRepet - nbPb)
+
+    parameters = {
+        "expName": expName, \
+        "nbRepet": nbRepet, \
+        "dualgap": dualgap, \
+        "nbIt": nbIt, \
+        "listM": listM, \
+        "listN": listN, \
+        "listLbd": listLbd, \
+        "listDico": listDico, \
+        "nbPoint": nbPoint, \
+        "tol_saturation": tol_saturation, \
+        "version": version, \
+    }
+
+    output_file = FOLDER + "Results/" + expName + ".pkl"
+
+    # Saving the objects:
+    with open(output_file, 'wb') as f:  # Python 3: open(..., 'wb')
+        pickle.dump([results_gap, results_st1, parameters], f)
+        #np.save(output_file, results)
+
+
+
+
+
---
+++ experiments/TSP2019/A_radius/vizu.py
@@ -0,0 +1,267 @@
+import yaml, os
+import numpy as np
+
+import argparse
+import pickle
+import matplotlib
+from packaging import version
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import matplotlib.collections as collections
+from matplotlib.legend_handler import HandlerBase
+
+import sys
+sys.path.insert(0,'antisparse_screening')
+
+class AnyObjectHandler(HandlerBase):
+    def create_artists(self, legend, orig_handle,
+                       x0, y0, width, height, fontsize, trans):
+        l1 = plt.Line2D([x0,y0+width], [0.7*height,0.7*height],
+                           linestyle='-', color=orig_handle[0],
+                           linewidth=2)
+        l2 = plt.Line2D([x0,y0+width], [0.3*height,0.3*height], 
+                           linestyle='--', color=orig_handle[1],
+                           linewidth=2)
+        return [l1, l2]
+
+
+FOLDER = os.path.dirname(os.path.realpath(__file__)) + '/'
+#FOLDER = "experiments/TSP/A_radius/"
+VERSION = "2"
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--save', help='save figure', action="store_true")
+parser.add_argument('--version', help='numero xp')
+args=parser.parse_args()
+
+blue_colors = np.array( [\
+    [116,169,207], \
+    [5,112,176], \
+    [2,56,88] \
+    ]) / 255.
+
+orange_colors = np.array( [\
+    [239,101,72], \
+    [215,48,31], \
+    [127,0,0] \
+    ]) / 255.
+
+sequence_color = np.array([
+    # Vert
+    [90,174,97], # Foncé
+    [27,120,55], # Moyen 
+    [0,68,27], # claire
+    # Violet
+    [153,112,171], # Foncé
+    [118,42,131], # Moyen 
+    [64,0,75], # claire
+    ]) / 255.
+
+
+
+
+
+
+
+
+
+# Load Results
+output_file = FOLDER + "Results/" + "radius3withST1" + ".pkl"
+
+with open(output_file, 'rb') as f:
+    [results_gap, results_st1, parameters] = pickle.load(f)
+
+expName  = str(parameters['expName'])
+nbRept   = int(parameters['nbRepet'])
+
+dualgap = float(parameters['dualgap'])
+nbIt    = int(parameters['nbIt'])
+
+listM = parameters['listM']
+listN = parameters['listN']
+listLbd = parameters['listLbd']
+listDico = parameters['listDico']
+
+nbPoint = int(parameters['nbPoint'])
+
+vers = str(parameters['version'])
+assert(version.parse(vers) >= version.parse(VERSION))
+
+ratio = 1.
+figsize = (ratio*4,ratio*4)
+range_radius = np.linspace(0.001, 2, nbPoint)
+fontsize = 15# 11 pour 16/9
+LINEWIDTH = 3
+
+SAVE_DIC_1 = 'pos_rand'
+SAVE_LBD_2 = .2
+
+def figs_one_dic():
+    # Display Results
+
+
+    for i_dico in range(len(listDico)):
+        dico = str(listDico[i_dico])
+
+        if dico != SAVE_DIC_1:
+            continue
+
+        for i_size in range(len(listM)):
+            m = int(listM[i_size])
+            n = int(listN[i_size])
+
+            f, ax = plt.subplots(1, 1, sharex=True, sharey=False, figsize=figsize)
+
+            listLines = []
+
+            for i_lbd in range(len(listLbd)):
+                lbd = float(listLbd[i_lbd])
+
+                # ax.set_title("Dico: " + dico \
+                #     + " size: "+ str([m,n])\
+                #     + " nbRet: " + str(nbRept))
+
+                if i_dico == 0:
+                    title = "positive dictionary"
+                elif i_dico == 1:
+                    title = "Gaussian dictionary"
+
+                #ax.set_title(title)
+
+                l1 = ax.plot(range_radius, 100. * results_gap[i_dico, i_size, i_lbd, :] / float(nbRept), \
+                    color=sequence_color[i_lbd, :], linewidth=LINEWIDTH, label='$\\lambda / \\lambda_{\\max}=' + str(lbd) + '$')
+                l2 = ax.plot(range_radius, 100. * results_st1[i_dico, i_size, i_lbd, :] / float(nbRept), \
+                    '--', color=sequence_color[3+i_lbd, :], linewidth=LINEWIDTH, \
+                    label='$\\lambda / \\lambda_{\\max}=' + str(lbd) + '$')
+
+                listLines.append([l1, l2])
+
+                #ax.set_xlim([range_radius[0], range_radius[-1]])
+                ax.set_xlim([range_radius[0], range_radius[-1]])
+                ax.set_ylim([0, 100])
+
+
+                deltax = 50*(range_radius[1] - range_radius[0])
+                line = np.array([.5, 100. * results_gap[i_dico, i_size, i_lbd, 300] / float(nbRept)])
+                deltay = 100 * (results_gap[i_dico, i_size, i_lbd, 300] \
+                    - results_gap[i_dico, i_size, i_lbd, 350]) / float(nbRept)
+                angle = np.arccos(deltax / np.sqrt(deltax**2 + deltay**2))
+                trans_angle = plt.gca().transData.transform_angles(np.array((np.rad2deg(angle),)), \
+                    line.reshape((1, 2)))[0]
+                # ax.text(.5, 1.1 * 100. * results_gap[i_dico, i_size, i_lbd, 300] / float(nbRept), \
+                #     '$\\lambda=' + str(lbd) + '$', \
+                #     fontsize=fontsize, color=sequence_color[i_lbd, :], \
+                #     rotation=-trans_angle, rotation_mode='anchor', \
+                #     bbox=dict(facecolor='white', edgecolor='white', pad=0., alpha=.8))
+
+            ax.set_xlabel('$r_0$', fontsize=fontsize+1)
+            ax.set_ylabel('% of detection', fontsize=fontsize+1)
+
+            labels = ['$\\lambda / \\lambda_{\\max}=' + str(lbd) + '$' for lbd in listLbd]
+            ax.legend([(sequence_color[0+i, :], sequence_color[3+i, :]) for i in range(3)], 
+                labels,
+                handler_map={tuple: AnyObjectHandler()},
+                fontsize=fontsize, frameon=False
+                )
+
+            for tick in ax.xaxis.get_major_ticks():
+                tick.label.set_fontsize(fontsize-2) 
+            for tick in ax.yaxis.get_major_ticks():
+                tick.label.set_fontsize(fontsize-2) 
+
+            for spine in ax.spines.values():
+                if spine.spine_type in ['top', 'right']:
+                    spine.set_visible(False)
+
+            if args.save and dico == SAVE_DIC_1:
+                name = '/exp_radius_dic' + dico + '_sizeM' + str(m) +'N' + str(n) + '.pdf'
+                f.savefig(FOLDER + name, dpi=None, facecolor='w', \
+                    edgecolor='w', orientation='portrait', \
+                    papertype=None, format=None, transparent=False, \
+                    bbox_inches='tight', pad_inches=0., \
+                    frameon=None, metadata=None)
+                
+            else:
+                pass
+                plt.show()
+
+
+def figs_one_lambda():
+
+    for i_lbd in range(len(listLbd)):
+        lbd = float(listLbd[i_lbd])
+
+        print(lbd)
+
+        for i_size in range(len(listM)):
+            m = int(listM[i_size])
+            n = int(listN[i_size])
+
+            f, ax = plt.subplots(1, 1, sharex=True, sharey=False, figsize=figsize)
+
+            for i_dico in range(len(listDico)):
+                dico = str(listDico[i_dico])
+                if dico == 'pos_rand':
+                    dico = "Uniform"
+                elif dico == 'norm':
+                    dico = "Gaussian"
+                elif dico == 'dct':
+                    dico = "DCT"
+
+                # ax.set_title("Dico: " + dico \
+                #     + " size: "+ str([m,n])\
+                #     + " nbRet: " + str(nbRept))
+
+                if i_dico == 0:
+                    title = "positive dictionary"
+                elif i_dico == 1:
+                    title = "Gaussian dictionary"
+
+                #ax.set_title(title)
+
+                ax.plot(range_radius, 100. * results_gap[i_dico, i_size, i_lbd, :] \
+                    / float(nbRept), \
+                    color=blue_colors[i_dico, :], linewidth=LINEWIDTH, label=dico)
+
+                if lbd == .1:
+                    ax.set_xlim([range_radius[0], .8])
+                elif lbd == .2:
+                    ax.set_xlim([range_radius[0], 1.25])
+                else:
+                    ax.set_xlim([range_radius[0], range_radius[-1]])
+                ax.set_ylim([0, 100])
+
+                for tick in ax.xaxis.get_major_ticks():
+                    tick.label.set_fontsize(fontsize-2) 
+                for tick in ax.yaxis.get_major_ticks():
+                    tick.label.set_fontsize(fontsize-2) 
+
+                ax.set_xlabel('$r_0$', fontsize=fontsize+1)
+                ax.set_ylabel('% of detection', fontsize=fontsize+1)
+
+                for spine in ax.spines.values():
+                    if spine.spine_type in ['top', 'right']:
+                        spine.set_visible(False)
+
+            ax.legend(fontsize=fontsize, frameon=False)
+
+            if args.save and lbd == SAVE_LBD_2:
+                print("ici")
+                name = '/exp_radius_lbd' + str(lbd).replace('.', '') + '_sizeM' \
+                + str(m) +'N' + str(n) + '.pdf'
+                f.savefig(FOLDER + name, dpi=None, facecolor='w', \
+                    edgecolor='w', orientation='portrait', \
+                    papertype=None, format=None, transparent=False, \
+                    bbox_inches='tight', pad_inches=0., \
+                    frameon=None, metadata=None)
+
+            else:
+                # print("ici")
+                plt.show()
+
+
+if __name__ == '__main__':
+
+    figs_one_dic()
+    # figs_one_lambda()
